c4ed1d723741b5f81d8531399a22154fb07411e4
Deepu <shreevastavdeepu068@gmail.com>
2026-06-22 09:12:55 +0530

    Refactor addToCart function and implement WhatsApp sharing

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// script.js
-let cartCount = 0;
-function addToCart() {
-    cartCount++;
-    document.getElementById("cart-count").innerText = cartCount;
-    alert("Juice Added to Cart!");
+);let cart = [];
+function addToCart(item, price) {
+    cart.push({item, price});
+    document.getElementById('cart-count').innerText = cart.length;
+    alert(item + " added to cart!");
 }
-
+function sendToWhatsApp() {
+    if(cart.length === 0) { alert("Cart is empty!"); return; }
+    let msg = "Hello, I want to order: " + cart.map(i => i.item).join(", ");
+    let url = "https://wa.me/917827543597?text=" + encodeURIComponent(msg);
+    window.open(url, '_blank');
+}
