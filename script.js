85577088a2a64ea108b29b7690355119cccaf7b8
Deepu <shreevastavdeepu068@gmail.com>
2026-06-22 09:50:34 +0530

    Refactor WhatsApp order function
    Replaced the sendToWhatsApp function with orderViaWhatsApp to streamline the ordering process.

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-);let cart = [];
-function addToCart(item, price) {
-    cart.push({item, price});
-    document.getElementById('cart-count').innerText = cart.length;
-    alert(item + " added to cart!");
-}
-function sendToWhatsApp() {
-    if(cart.length === 0) { alert("Cart is empty!"); return; }
-    let msg = "Hello, I want to order: " + cart.map(i => i.item).join(", ");
-    let url = "https://wa.me/917827543597?text=" + encodeURIComponent(msg);
+function orderViaWhatsApp(item, price) {
+    let phone = "917827543597";
+    let url = `https://wa.me/${phone}?text=Hello, I want to order ${item} for ₹${price}`;
     window.open(url, '_blank');
 }
